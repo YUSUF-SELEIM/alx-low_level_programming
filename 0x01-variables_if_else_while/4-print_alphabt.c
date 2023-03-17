eb8c0226b34de34cc3541f770f19524898d49e7d
YuSuF_Abdelfattah <97110015+JOEGREEEN@users.noreply.github.com>
2023-03-17 19:44:17 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
+  for (i = 'a' ; i <= 'z' ; i++)
   {
-    if (i == 'e' || i == 'q')
+    if (i != 'e' || i != 'q')
     {
         putchar(i);
     }
-	}
+  }
 	putchar('\n');
 	return (0);
 }
