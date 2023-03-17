35f6c4b0fe02031e4b9d715acce811b37286f3c5
YuSuF_Abdelfattah <97110015+JOEGREEEN@users.noreply.github.com>
2023-03-17 19:29:30 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
 
+	for (i = 'a' ; i <= 'Z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
