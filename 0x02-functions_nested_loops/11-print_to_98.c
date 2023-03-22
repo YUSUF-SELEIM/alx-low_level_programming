a85e404d082c5d61056da62a4a68a31f34215d4b
JOEGREEEN <yusufabdelfattah207@gmail.com>
2023-03-21 18:03:00 -0700

    Task 11 mod

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -30,13 +30,14 @@ void print_to_98(int n)
 	{	int i;
 
 		for (i = n ; i >= 98 ; i--)
-		{	
+		{
 			if (i == 98)
 			{
 				printf("%d\n", i);
 			} else
 			{
 				printf("%d, ", i);
+			}
 		}
 	}
 }
