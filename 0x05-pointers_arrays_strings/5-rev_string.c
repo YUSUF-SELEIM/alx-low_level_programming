28ae71ae90b536efd9abd7211aca38a194b30ee8
JOEGREEEN <yusufabdelfattah207@gmail.com>
2023-03-27 14:31:44 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to the string to print
+ */
+void print_rev(char *s)
+{
+	char tmp;
+	int i, len1;
+
+	len1 = 0;
+
+	len1 = strlen(s) - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
+}
