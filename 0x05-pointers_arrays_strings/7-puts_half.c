a6b6f075dbfd41b7f9884e0866877f13cad5aca3
JOEGREEEN <yusufabdelfattah207@gmail.com>
2023-03-27 14:53:20 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to the string to print
+ */
+void puts_half(char *str)
+{
+	if (strlen(str) % 2 != 0)
+	{
+		int i;
+		int len;
+
+		len = (int)(strlen(str) - 1) / 2;
+		
+		for (i = 0 ; i <= len ; i++ )
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	} else 
+	{
+		int i;
+		int len;
+
+		len = (int)(strlen(str) / 2);
+
+		for (i = 0 ; i <= len ; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+}
